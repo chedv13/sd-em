a36acb3be11bdfa9e38a95436e84d41b0a603280
Dmitry Chekurov <chedv13@gmail.com>
2022-04-18 01:39:06 +0400

    updated webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,24 @@
 const path = require('path');
 
-const config1 = {
-    entry: "./src/index.ts",
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: "index.js",
-        library: 'SD',
-        libraryTarget: 'var'
-    },
-    devtool: "source-map",
-    resolve: {
-        extensions: ["", ".webpack.js", ".web.js", ".ts", ".tsx", ".js"],
-    },
+module.exports = {
+    entry: './src/index.ts',
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                loader: "ts-loader"
-            },
-            {
-                test: /\.js$/,
-                loader: "source-map-loader"
+                use: 'ts-loader',
+                exclude: /node_modules/,
             },
         ],
-    }
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist'),
+        library: 'SD',
+        libraryTarget: 'umd'
+    },
+    target: ['web', 'es2015'],
 };
-
-module.exports = [
-    config1
-];
